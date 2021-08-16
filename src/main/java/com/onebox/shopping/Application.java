9b2d510aadf4e04a5845f79caf59e695055e8da4
manel1983 <manel1983@users.noreply.github.com>
2021-08-16 16:36:12 +0200

    Remove spring cache

--- src/main/java/com/onebox/shopping/Application.java
+++ src/main/java/com/onebox/shopping/Application.java
@@ -4,13 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableCaching
 public class Application {
 	
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
